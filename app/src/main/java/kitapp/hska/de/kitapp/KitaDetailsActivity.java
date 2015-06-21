2cc03a6e59da0add6673891f91b43cc6463fde6a
Yannick Gentner <y.gentner@gmail.com>
2015-06-21 23:44:35 +0200

    openhours out

--- app/src/main/java/kitapp/hska/de/kitapp/KitaDetailsActivity.java
+++ app/src/main/java/kitapp/hska/de/kitapp/KitaDetailsActivity.java
@@ -139,12 +139,12 @@ public class KitaDetailsActivity extends AppCompatActivity {
         }
 
         if (kita.getOpeningHours() != null) {
-            kitaDetailsOpenhours.setText(kita.getOpeningHours().getText());
+            //kitaDetailsOpenhours.setText(kita.getOpeningHours().getText());//TODO
         }
 
 
         kitaDetailsManagement.setText(kita.getManagement());
-        kitaDetailsConfessionData.setText(kita.getConfession().getText());
+        //kitaDetailsConfessionData.setText(kita.getConfession().getText()); //TODO
         kitaDetailsAgeData.setText(kita.getMinAge() + " - " + kita.getMaxAge());
         kitaDetailsAboutData.setText(kita.getAbout());
     }
